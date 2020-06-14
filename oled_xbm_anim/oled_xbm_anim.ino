df948868bfe1cd9c8112aa450eea9cc39d39e089
Don Williams <31633408+donxw@users.noreply.github.com>
2020-06-14 14:19:53 -0700

    Add files via upload

--- oled_xbm_anim/oled_xbm_anim.ino
+++ oled_xbm_anim/oled_xbm_anim.ino
@@ -16,7 +16,7 @@ U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ 16, /* clock=*/
 //U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ 16, /* clock=*/ 15, /* data=*/ 4);  //heltec wifikit32
 //U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 2, /* data=*/ 0);  //node mcu with D3 (gpio0) data and D4 (gpio2) on clk and no reset pin - page buffer
 
-const int AnimationDelay = 500; //10FPS
+const int AnimationDelay = 400; // larger delay = slower frame animation
 bool animation_direction = false;
 int animation_state = 0;
 
@@ -50,16 +50,16 @@ void drawAnimation() {
   do {
     switch (animation_state) {
       case 0:
-        u8g2.drawXBMP(0, 0, frames_width, frames_height, testXbm0_bits);
-        //u8g2.drawXBMP(0, 0, frames_width, frames_height, blinkImg[0]);
+        //u8g2.drawXBMP(0, 0, frames_width, frames_height, testXbm0_bits);
+        u8g2.drawXBMP(0, 0, frames_width, frames_height, blinkImg[0]);
         break;
       case 1:
-        u8g2.drawXBMP(0, 0, frames_width, frames_height, testXbm1_bits);
-        //u8g2.drawXBMP(0, 0, frames_width, frames_height, blinkImg[1]);
+        //u8g2.drawXBMP(0, 0, frames_width, frames_height, testXbm1_bits);
+        u8g2.drawXBMP(0, 0, frames_width, frames_height, blinkImg[1]);
         break;
       case 2:
-        u8g2.drawXBMP(0, 0, frames_width, frames_height, testXbm2_bits);
-        //u8g2.drawXBMP(0, 0, frames_width, frames_height, blinkImg[2]);
+        //u8g2.drawXBMP(0, 0, frames_width, frames_height, testXbm2_bits);
+        u8g2.drawXBMP(0, 0, frames_width, frames_height, blinkImg[2]);
         break;
     }
   } while ( u8g2.nextPage() );
