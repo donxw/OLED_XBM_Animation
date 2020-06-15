e8cd6e5d6acf18fdd6c35172ef45f9bb3c276cd9
Don Williams <31633408+donxw@users.noreply.github.com>
2020-06-15 10:31:58 -0700

    Add files via upload

--- oled_xbm_anim/oled_xbm_anim.ino
+++ oled_xbm_anim/oled_xbm_anim.ino
@@ -2,7 +2,9 @@
    OLED Animation Example
    Modified from code posted at:  https://www.hackster.io/138689/pixel-art-on-oled-display-7f8697#code
    GIMP used to create XBM files in frames.h
-   board:  NodeMCU 1.0
+   boards tested:  
+      NodeMCU 1.0 for Heltec wifikit8 
+      Heltec Wifi Kit 32
 */
 
 #include <Wire.h> //I2C
@@ -13,9 +15,9 @@
 AsyncDelay tick;
 
 //U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ 16, /* clock=*/ 5, /* data=*/ 4);  //heltek wifikit8
+//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ 16, /* clock=*/ 5, /* data=*/ 4);  //heltek wifikit8
 //U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);  //heltec wifikit32 page mode
-//U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ 16, /* clock=*/ 15, /* data=*/ 4);  //heltec wifikit32
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ 16, /* clock=*/ 15, /* data=*/ 4);  //heltec wifikit32
 //U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 2, /* data=*/ 0);  //node mcu with D3 (gpio0) data and D4 (gpio2) on clk and no reset pin - page buffer
 
 const int AnimationDelay = 400; // larger delay = slower frame animation
@@ -25,6 +27,11 @@ int current_frame = 0;
 void setup() {
   u8g2.begin();
   tick.start(AnimationDelay, AsyncDelay::MILLIS);
+
+  #ifdef WIFI_Kit_32
+  Wire.begin(4, 15);  //required for wifikit 32 only (breaks wifikit 8)
+  Serial.print("wire.begin(4, 15) invoked");
+#endif
 }
 
 void loop() {
